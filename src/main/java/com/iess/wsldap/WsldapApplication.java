8a78c3ec1d79edf885b93b82493f87a0608bafb1
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-09-23 09:46:56 -0500

    first commit

---
+++ src/main/java/com/iess/wsldap/WsldapApplication.java
@@ -0,0 +1,13 @@
+package com.iess.wsldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsldapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsldapApplication.class, args);
+	}
+
+}
